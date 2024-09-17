04654ccbd9639ab3c33c74875f69fc40b92a8e82
vsmart-06 <srivishnuvusirikala@gmail.com>
2024-09-17 11:43:51 -0500

    Initialized api code

---
+++ api/main.py
@@ -0,0 +1,40 @@
+from flask import Flask, request
+from records import HousePoints
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
+MONGO_LINK = os.getenv("MONGO_LINK")
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<marquee direction = 'left'><h1>Welcome to Hogwarts!</h1></marquee>"
+
+@app.route("/add-points", methods = ["POST"])
+def add_points():
+    data = request.form
+    command = data.get("command")
+    db = HousePoints(MONGO_LINK)
+
+    try:
+        if " points to " in command:
+            a, b = command.split(" points to ")
+            points = [int(a), b]
+
+        elif " points from " in command:
+            a, b = command.split(" points from ")
+            points = [-int(a), b]
+        
+        else:
+            return {"error": "Invalid Command"}
+        
+    except:
+        return {"error": "Invalid Command"}
+
+    db.modify_points(points[0], points[1])
+
+    return {"message": "Points successfully changed"}
+    
